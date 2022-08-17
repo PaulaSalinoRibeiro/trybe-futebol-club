407c74109e0a012ef7e958206a16e90484951870
Paula <paulasalinor@gmail.com>
2022-08-17 17:15:05 -0300

    add user migrate

---
+++ app/backend/src/database/migrations/202208171557-users.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
