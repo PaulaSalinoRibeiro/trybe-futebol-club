4ca3be9750ad961b25997325584dd96a92807352
Paula <paulasalinor@gmail.com>
2022-08-17 17:16:09 -0300

    add team model

---
+++ app/backend/src/database/models/team.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'team',
+  timestamps: false,
+  underscored: true,
+});
+
+export default Team;
