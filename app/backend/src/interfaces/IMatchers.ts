4997cf6a58459df6e806be0d864fab5733352245
Paula <paulasalinor@gmail.com>
2022-08-18 16:54:36 -0300

    add IMatchers interface

---
+++ app/backend/src/interfaces/IMatchers.ts
@@ -0,0 +1,14 @@
+export default interface IMatchers {
+  id: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+  teamHome: { teamName: string };
+  teamAway: { teamName: string };
+}
+
+export interface Matchers {
+  getAllMatchers(): Promise<IMatchers[]>
+}
