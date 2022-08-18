faff61573b2194b02188ad64a59db5cfca3e9efb
Paula <paulasalinor@gmail.com>
2022-08-18 08:51:41 -0300

    add loginValidate method

--- app/backend/src/routers/login.ts
+++ app/backend/src/routers/login.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import LoginService from '../services/loginService';
 import LoginController from '../controllers/loginController';
+import tokenMiddleware from '../middlewares/tokenMiddleware';
 
 const loginRouter = express.Router();
 
@@ -8,5 +9,12 @@ const loginService = new LoginService();
 const loginController = new LoginController(loginService);
 
 loginRouter.post('/login', (req, res, next) => loginController.login(req, res, next));
+
+loginRouter.use(tokenMiddleware);
+
+loginRouter.get(
+  '/login/validate',
+  (req, res, next) => LoginController.loginValidate(req, res, next),
+);
 
 export default loginRouter;
