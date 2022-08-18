ceb44812846342cf0de084ab8f57a1ae59c7b937
Paula <paulasalinor@gmail.com>
2022-08-17 22:12:54 -0300

    add LoginController

---
+++ app/backend/src/controllers/loginController.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import Login from '../interfaces/ILogin';
+
+export default class LoginController {
+  constructor(private loginService: Login) {
+    this.loginService = loginService;
+  }
+
+  async login(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+    try {
+      const token = await this.loginService.login(req.body);
+      res.status(StatusCodes.OK).json({ token });
+    } catch (err) {
+      next(err);
+    }
+  }
+}
