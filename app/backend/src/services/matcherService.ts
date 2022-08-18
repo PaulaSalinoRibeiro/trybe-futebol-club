9bcdb222b8b4404d5a7850d69657c905104db454
Paula <paulasalinor@gmail.com>
2022-08-18 16:55:58 -0300

    add getAllMatchers method in MatcherService class

---
+++ app/backend/src/services/matcherService.ts
@@ -0,0 +1,24 @@
+import Team from '../database/models/team';
+import Matches from '../database/models/matches';
+
+import IMatchers from '../interfaces/IMatchers';
+
+export default class MatcherService {
+  constructor(private model = Matches) {
+    this.model = model;
+  }
+
+  async getAllMatchers(): Promise<IMatchers[]> {
+    const matchers = await this.model.findAll({
+      include: [
+        { model: Team, as: 'teamHome', attributes: ['teamName'] },
+        { model: Team, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
+    return matchers as unknown as IMatchers[];
+  }
+
+  // async getByFilter(query: string): Promise<IMatchers | void> {
+
+  // }
+}
