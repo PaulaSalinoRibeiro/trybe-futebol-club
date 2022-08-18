bb2e4a366d9694289ddf507ab8e46f13b31cde55
Paula <paulasalinor@gmail.com>
2022-08-18 12:28:01 -0300

    add teams in app class

--- app/backend/src/routers/index.ts
+++ app/backend/src/routers/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import loginRouter from './login';
+import teamsRouter from './teams';
 
 const router = Router();
 
+router.use(teamsRouter);
 router.use(loginRouter);
 
 export default router;
