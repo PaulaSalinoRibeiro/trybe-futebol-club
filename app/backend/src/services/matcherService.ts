c972363ecebe507acb13d6d13570288cdb62bf15
Paula <paulasalinor@gmail.com>
2022-08-19 10:07:48 -0300

    add create method in MatcherService class

--- app/backend/src/services/matcherService.ts
+++ app/backend/src/services/matcherService.ts
@@ -1,7 +1,9 @@
+import { StatusCodes } from 'http-status-codes';
 import Team from '../database/models/team';
 import Matches from '../database/models/matches';
+import HandleError from '../utils/handleError';
 
-import IMatchers from '../interfaces/IMatchers';
+import IMatchers, { MatchersCreate } from '../interfaces/IMatchers';
 
 export default class MatcherService {
   constructor(private model = Matches) {
@@ -18,16 +20,28 @@ export default class MatcherService {
     return matchers as unknown as IMatchers[];
   }
 
-  // async getByFilter(query: string): Promise<IMatchers[]> {
-  //   const matchers = await this.model.findAll({
-  //     where: {
-  //       inProgress: query,
-  //     },
-  //     include: [
-  //       { model: Team, as: 'teamHome', attributes: ['teamName'] },
-  //       { model: Team, as: 'teamAway', attributes: ['teamName'] },
-  //     ],
-  //   });
-  //   return matchers as unknown as IMatchers[];
-  // }
+  async create(data: MatchersCreate): Promise<MatchersCreate> {
+    if (data.homeTeam === data.awayTeam) {
+      throw new HandleError(
+        StatusCodes.UNAUTHORIZED,
+        'It is not possible to create a match with two equal teams',
+      );
+    }
+
+    const homeTeam = await Team.findOne({ where: { id: data.homeTeam } });
+
+    const awayTeam = await Team.findOne({ where: { id: data.awayTeam } });
+
+    if (!homeTeam || !awayTeam) {
+      throw new HandleError(StatusCodes.NOT_FOUND, 'There is no team with such id!');
+    }
+
+    const newMatcher = await this.model.create({ ...data, inProgress: 1 });
+
+    return newMatcher;
+  }
+
+  async updateProgress(id: number): Promise<void> {
+    await this.model.update({ inProgress: false }, { where: { id } });
+  }
 }
