4ffb5460df973b8d395b071cb18f693988695584
Paula <paulasalinor@gmail.com>
2022-08-19 10:09:12 -0300

    add routers patch /:id/finish and post /

--- app/backend/src/routers/matchers.ts
+++ app/backend/src/routers/matchers.ts
@@ -1,11 +1,24 @@
 import * as express from 'express';
 import MatcherService from '../services/matcherService';
 import MatcherController from '../controllers/matcherController';
+import tokenMiddleware from '../middlewares/tokenMiddleware';
 
 const matcherService = new MatcherService();
 const matcherController = new MatcherController(matcherService);
 
 const matchersRouter = express.Router();
+
+matchersRouter.patch(
+  '/:id/finish',
+  tokenMiddleware,
+  (req, res, next) => matcherController.updateProgress(req, res, next),
+);
+
+matchersRouter.post(
+  '/',
+  tokenMiddleware,
+  (req, res, next) => matcherController.create(req, res, next),
+);
 
 matchersRouter.get(
   '/',
