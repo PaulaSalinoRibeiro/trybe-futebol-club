2fa2c0665f12f21f17b203d11d74a805a56a81be
Paula <paulasalinor@gmail.com>
2022-08-22 18:22:39 -0300

    add mock

---
+++ app/backend/src/tests/mock/getFinishedMatchers.ts
@@ -0,0 +1,124 @@
+export const queryFinishedMatchersMock = [
+  {
+    "id": 16,
+    "teamName": "São Paulo",
+    "awayTeam": [
+      {
+        "id": 33,
+        "homeTeam": 1,
+        "homeTeamGoals": 1,
+        "awayTeam": 16,
+        "awayTeamGoals": 1,
+        "inProgress": false
+      },
+      {
+        "id": 23,
+        "homeTeam": 15,
+        "homeTeamGoals": 2,
+        "awayTeam": 16,
+        "awayTeamGoals": 3,
+        "inProgress": false
+      },
+      {
+        "id": 14,
+        "homeTeam": 14,
+        "homeTeamGoals": 2,
+        "awayTeam": 16,
+        "awayTeamGoals": 1,
+        "inProgress": false
+      }
+    ],
+    "homeTeam": [
+      {
+        "id": 1,
+        "homeTeam": 16,
+        "homeTeamGoals": 1,
+        "awayTeam": 8,
+        "awayTeamGoals": 1,
+        "inProgress": false
+      },
+      {
+        "id": 28,
+        "homeTeam": 16,
+        "homeTeamGoals": 3,
+        "awayTeam": 7,
+        "awayTeamGoals": 0,
+        "inProgress": false
+      }
+    ]
+  },
+  {
+    "id": 9,
+    "teamName": "Internacional",
+    "awayTeam": [
+      {
+        "id": 20,
+        "homeTeam": 7,
+        "homeTeamGoals": 0,
+        "awayTeam": 9,
+        "awayTeamGoals": 1,
+        "inProgress": false
+      },       
+      { "id": 10,
+        "homeTeam": 2,
+        "homeTeamGoals": 0,
+        "awayTeam": 9,
+        "awayTeamGoals": 2,
+        "inProgress": false
+      }
+    ],
+    "homeTeam": [
+        {
+          "id": 2,
+          "homeTeam": 9,
+          "homeTeamGoals": 1,
+          "awayTeam": 14,
+          "awayTeamGoals": 1,
+          "inProgress": false
+        },
+        {
+          "id": 29,
+          "homeTeam": 9,
+          "homeTeamGoals": 0,
+          "awayTeam": 4,
+          "awayTeamGoals": 4,
+          "inProgress": false
+        },
+        {
+          "id": 34,
+          "homeTeam": 9,
+          "homeTeamGoals": 3,
+          "awayTeam": 6,
+          "awayTeamGoals": 1,
+          "inProgress": false
+        }
+    ]
+  },
+]
+
+export const resultFinishedMatchersMock = [
+  {
+    "name": "Internacional",
+    "totalPoints": 10,
+    "totalGames": 5,
+    "totalVictories": 3,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 7,
+    "goalsOwn": 6,
+    "goalsBalance": 1,
+    "efficiency": 66.67
+  },
+  {
+    "name": "São Paulo",
+    "totalPoints": 8,
+    "totalGames": 5,
+    "totalVictories": 2,
+    "totalDraws": 2,
+    "totalLosses": 1,
+    "goalsFavor": 9,
+    "goalsOwn": 6,
+    "goalsBalance": 3,
+    "efficiency": 53.33
+  },
+]
