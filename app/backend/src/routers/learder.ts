83705df8640cce3f89751ab5978fc0369eebdeef
Paula <paulasalinor@gmail.com>
2022-08-22 16:30:03 -0300

    add router get/leaderboard/away

--- app/backend/src/routers/learder.ts
+++ app/backend/src/routers/learder.ts
@@ -7,6 +7,14 @@ const leaderController = new LeaderBoardController(leaderService);
 
 const leaderRouter = express.Router();
 
-leaderRouter.get('/home', (req, res, next) => leaderController.getFinishedMatchers(req, res, next));
+leaderRouter.get(
+  '/home',
+  (req, res, next) => leaderController.getFinishedMatchersHome(req, res, next),
+);
+
+leaderRouter.get(
+  '/away',
+  (req, res, next) => leaderController.getFinishedMatchersAway(req, res, next),
+);
 
 export default leaderRouter;
