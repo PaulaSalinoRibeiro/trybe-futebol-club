68db5994801fd7cb8779e61c3189028cd0fa755a
Paula <paulasalinor@gmail.com>
2022-08-22 17:26:53 -0300

    add getFinishedmatchers method

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -2,14 +2,19 @@ import Matches from '../database/models/matches';
 import Team from '../database/models/team';
 
 import { MatchersCreate } from '../interfaces/IMatchers';
-import { MatchesTeam, ILeaderBoard, MatchesTeamAway } from '../interfaces/ILeaderBoard';
+import {
+  MatchesTeamHome,
+  ILeaderBoard,
+  MatchesTeamAway,
+  MatchesTeam,
+} from '../interfaces/ILeaderBoard';
 
 export default class LeaderBoardService {
   constructor(private model = Team) {
     this.model = model;
   }
 
-  static resultsGoalsHome(matchers: MatchersCreate[]): number[] {
+  resultsGoalsHome = (matchers: MatchersCreate[]): number[] => {
     const goalsFavorHome = matchers
       .reduce((acc, curr: MatchersCreate) => acc + curr.homeTeamGoals, 0);
 
@@ -17,9 +22,9 @@ export default class LeaderBoardService {
       .reduce((acc, curr: MatchersCreate) => acc + curr.awayTeamGoals, 0);
 
     return [goalsFavorHome, goalsOwnHome];
-  }
+  };
 
-  static resultsGoalsAway(matchers: MatchersCreate[]): number[] {
+  resultsGoalsAway = (matchers: MatchersCreate[]): number[] => {
     const goalsFavorAway = matchers
       .reduce((acc, curr: MatchersCreate) => acc + curr.awayTeamGoals, 0);
 
@@ -27,27 +32,27 @@ export default class LeaderBoardService {
       .reduce((acc, curr: MatchersCreate) => acc + curr.homeTeamGoals, 0);
 
     return [goalsFavorAway, goalsOwnAway];
-  }
+  };
 
-  static resultOfMatchersHome(matchers: MatchersCreate[]): number[] {
-    let victories = 0;
-    let draws = 0;
-    let losses = 0;
+  resultOfMatchersHome = (matchers: MatchersCreate[]): number[] => {
+    let victoriesHome = 0;
+    let drawsHome = 0;
+    let lossesHome = 0;
 
     matchers.forEach(({ homeTeamGoals, awayTeamGoals }) => {
-      if (homeTeamGoals > awayTeamGoals) victories += 1;
+      if (homeTeamGoals > awayTeamGoals) victoriesHome += 1;
 
-      if (homeTeamGoals === awayTeamGoals) draws += 1;
+      if (homeTeamGoals === awayTeamGoals) drawsHome += 1;
 
-      if (homeTeamGoals < awayTeamGoals) losses += 1;
+      if (homeTeamGoals < awayTeamGoals) lossesHome += 1;
     });
 
-    const totalPoints = 3 * victories + draws;
+    const totalPoints = 3 * victoriesHome + drawsHome;
 
-    return [victories, draws, losses, totalPoints];
-  }
+    return [victoriesHome, drawsHome, lossesHome, totalPoints];
+  };
 
-  static resultOfMatchersAway(matchers: MatchersCreate[]): number[] {
+  resultOfMatchersAway = (matchers: MatchersCreate[]): number[] => {
     let victories = 0;
     let draws = 0;
     let losses = 0;
@@ -63,14 +68,14 @@ export default class LeaderBoardService {
     const totalPoints = 3 * victories + draws;
 
     return [victories, draws, losses, totalPoints];
-  }
+  };
 
-  static formattedHome({ teamName, homeTeam }: MatchesTeam) {
+  formattedHome = ({ teamName, homeTeam }: MatchesTeamHome) => {
     const [
       victoriesHome, drawsHome, lossesHome, pointsHome,
-    ] = LeaderBoardService.resultOfMatchersHome(homeTeam);
+    ] = this.resultOfMatchersHome(homeTeam);
 
-    const [goalsFavorHome, goalsOwnHome] = LeaderBoardService.resultsGoalsHome(homeTeam);
+    const [goalsFavorHome, goalsOwnHome] = this.resultsGoalsHome(homeTeam);
     return {
       name: teamName,
       totalPoints: pointsHome,
@@ -83,31 +88,56 @@ export default class LeaderBoardService {
       goalsBalance: (goalsFavorHome - goalsOwnHome),
       efficiency: Number(((pointsHome / (homeTeam.length * 3)) * 100).toFixed(2)),
     };
-  }
+  };
 
-  static formattedAway({ teamName, awayTeam }: MatchesTeamAway) {
+  formattedAway = ({ teamName, awayTeam }: MatchesTeamAway) => {
     const [
-      victoriesHome, drawsHome, lossesHome, pointsHome,
-    ] = LeaderBoardService.resultOfMatchersAway(awayTeam);
+      victoriesAway, drawsAway, lossesAway, pointsAway,
+    ] = this.resultOfMatchersAway(awayTeam);
 
-    const [goalsFavorAway, goalsOwnAway] = LeaderBoardService.resultsGoalsAway(awayTeam);
+    const [goalsFavorAway, goalsOwnAway] = this.resultsGoalsAway(awayTeam);
     return {
       name: teamName,
-      totalPoints: pointsHome,
+      totalPoints: pointsAway,
       totalGames: awayTeam.length,
-      totalVictories: victoriesHome,
-      totalDraws: drawsHome,
-      totalLosses: lossesHome,
+      totalVictories: victoriesAway,
+      totalDraws: drawsAway,
+      totalLosses: lossesAway,
       goalsFavor: goalsFavorAway,
       goalsOwn: goalsOwnAway,
       goalsBalance: (goalsFavorAway - goalsOwnAway),
-      efficiency: Number(((pointsHome / (awayTeam.length * 3)) * 100).toFixed(2)),
+      efficiency: Number(((pointsAway / (awayTeam.length * 3)) * 100).toFixed(2)),
     };
-  }
+  };
+
+  formatted = ({ teamName, awayTeam, homeTeam }: MatchesTeam) => {
+    const [victoriesHome, drawsHome, lossesHome, pointsHome] = this.resultOfMatchersHome(homeTeam);
+
+    const [goalsFavorHome, goalsOwnHome] = this.resultsGoalsHome(homeTeam);
+
+    const [victoriesAway, drawsAway, lossesAway, pointsAway] = this.resultOfMatchersAway(awayTeam);
+
+    const [goalsFavorAway, goalsOwnAway] = this.resultsGoalsAway(awayTeam);
+
+    return {
+      name: teamName,
+      totalPoints: pointsHome + pointsAway,
+      totalGames: [...homeTeam, ...awayTeam].length,
+      totalVictories: victoriesHome + victoriesAway,
+      totalDraws: drawsHome + drawsAway,
+      totalLosses: lossesHome + lossesAway,
+      goalsFavor: goalsFavorHome + goalsFavorAway,
+      goalsOwn: goalsOwnHome + goalsOwnAway,
+      goalsBalance: ((goalsFavorHome + goalsFavorAway) - (goalsOwnHome + goalsOwnAway)),
+      efficiency: Number(
+        (((pointsHome + pointsAway) / ([...homeTeam, ...awayTeam].length * 3)) * 100).toFixed(2),
+      ),
+    };
+  };
 
   // ref. https://stackoverflow.com/questions/6913512/how-to-sort-an-array-of-objects-by-multiple-fields
 
-  static orderMatchers(a: ILeaderBoard, b: ILeaderBoard) {
+  orderMatchers = (a: ILeaderBoard, b: ILeaderBoard) => {
     if (a.totalPoints < b.totalPoints) { return 1; }
     if (a.totalPoints > b.totalPoints) { return -1; }
     if (a.totalVictories < b.totalVictories) { return 1; }
@@ -119,7 +149,7 @@ export default class LeaderBoardService {
     if (a.goalsOwn < b.goalsOwn) { return 1; }
     if (a.goalsOwn > b.goalsOwn) { return -1; }
     return 0;
-  }
+  };
 
   async getFinishedMatchersHome(): Promise<ILeaderBoard[]> {
     const matchers = await this.model.findAll({
@@ -128,11 +158,11 @@ export default class LeaderBoardService {
       ],
     });
 
-    const fineshedMatches = matchers as unknown as MatchesTeam[];
+    const fineshedMatches = matchers as unknown as MatchesTeamHome[];
 
-    const leaderBoardHome = fineshedMatches.map(LeaderBoardService.formattedHome);
+    const leaderBoardHome = fineshedMatches.map(this.formattedHome);
 
-    return leaderBoardHome.sort(LeaderBoardService.orderMatchers);
+    return leaderBoardHome.sort(this.orderMatchers);
   }
 
   async getFinishedmatchersAway(): Promise<ILeaderBoard[]> {
@@ -144,8 +174,23 @@ export default class LeaderBoardService {
 
     const fineshedMatches = matchers as unknown as MatchesTeamAway[];
 
-    const leaderBoardHome = fineshedMatches.map(LeaderBoardService.formattedAway);
+    const leaderBoardHome = fineshedMatches.map(this.formattedAway);
 
-    return leaderBoardHome.sort(LeaderBoardService.orderMatchers);
+    return leaderBoardHome.sort(this.orderMatchers);
+  }
+
+  async getFinishedMatchers(): Promise<ILeaderBoard[]> {
+    const matchers = await this.model.findAll({
+      include: [
+        { model: Matches, as: 'awayTeam', where: { inProgress: 0 } },
+        { model: Matches, as: 'homeTeam', where: { inProgress: 0 } },
+      ],
+    });
+
+    const fineshedMatches = matchers as unknown as MatchesTeam[];
+
+    const leadeBoard = fineshedMatches.map(this.formatted);
+
+    return leadeBoard.sort(this.orderMatchers);
   }
 }
